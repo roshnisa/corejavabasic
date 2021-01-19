213cddba3f157e427c2957d49c294926f5f88715
roshnisa <salunke1292@gmail.com>
2021-01-19 03:52:31 -0800

    initial version

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+package basic.in;
+
+/**
+ * perform multiplication 
+ *
+ */
+public class Multiplication {
+	public static void main(String[] args) {
+		int x=4;
+		int y=8;
+		int z=x*y;
+		System.out.println("multiplie = " +z);
+		}
+
+}
